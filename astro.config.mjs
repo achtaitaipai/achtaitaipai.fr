5d4a238a70984d085fbf6ee81c67f27a9a8306c5
achtaitaipai <charles.cailleteau@gmail.com>
2024-08-25 20:28:24 +0200

    feat sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,23 @@
 import { defineConfig } from "astro/config";
-import expressiveCode from "astro-expressive-code"
+import expressiveCode from "astro-expressive-code";
 import remarkUnwrapImages from "remark-unwrap-images";
 import mdx from "@astrojs/mdx";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://achtaitaipai.fr",
   markdown: {
-    remarkPlugins: [remarkUnwrapImages],
+    remarkPlugins: [remarkUnwrapImages]
   },
-  integrations: [
-    expressiveCode({
-      themeCssSelector: (theme) =>
-        theme.name.includes("light")
-          ? ''
-          : '.dark',
-      styleOverrides: {
-        borderRadius: 0,
-        frames: {
-          shadowColor: "transparent",
-        },
-      },
-    }),
-    mdx(),
-  ],
+  integrations: [expressiveCode({
+    themeCssSelector: theme => theme.name.includes("light") ? '' : '.dark',
+    styleOverrides: {
+      borderRadius: 0,
+      frames: {
+        shadowColor: "transparent"
+      }
+    }
+  }), mdx(), sitemap()]
 });
