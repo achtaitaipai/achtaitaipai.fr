8ea0c3e5f17fbd133700ac49614032a7ad7c3a05
achtaitaipai <charles.cailleteau@gmail.com>
2024-08-26 15:17:09 +0200

    feat categories

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content"
+import { defineCollection, z,reference } from "astro:content"
 
 export const collections = {
 	posts: defineCollection({
@@ -7,9 +7,17 @@ export const collections = {
 			title: z.string(),
 			date: z.date(),
 			description: z.string().optional(),
+			categories: z.array(reference("categories")).optional()
 		}),
 	}),
 	pages: defineCollection({
+		type: "content",
+		schema: z.object({
+			title: z.string()
+		})
+	}),
+
+	categories: defineCollection({
 		type: "content",
 		schema: z.object({
 			title: z.string()
