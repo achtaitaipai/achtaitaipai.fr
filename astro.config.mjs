dc0fd9a2ee330004e3d6087b5a9dc5f307d8cd9a
achtaitaipai <charles.cailleteau@gmail.com>
2024-11-06 22:32:42 +0100

    without server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
-import node from '@astrojs/node';
-
 // https://astro.build/config
-export default defineConfig({
-  output: 'hybrid',
+export default defineConfig({})
 
-  adapter: node({
-    mode: 'standalone',
-  }),
-})
