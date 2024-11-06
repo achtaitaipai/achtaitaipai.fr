c3f6c9bcc1262f66474646db75b2639f1fb754c7
achtaitaipai <charles.cailleteau@gmail.com>
2024-11-06 20:37:39 +0100

    remove alpinejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
-import alpinejs from '@astrojs/alpinejs'
-
 // https://astro.build/config
-export default defineConfig({
-	integrations: [alpinejs()],
-})
+export default defineConfig({})
