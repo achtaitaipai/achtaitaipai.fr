c0e7cdcb07e9612627f78253bb02cdc6452fdee4
achtaitaipai <charles.cailleteau@gmail.com>
2024-11-06 20:36:15 +0100

    feat prettier

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,6 +1,6 @@
 export function displayDate(date: Date) {
-  return new Intl.DateTimeFormat("fr-FR", {
-    dateStyle: "long",
-    timeZone: "Europe/Paris",
-  }).format(date);
+	return new Intl.DateTimeFormat('fr-FR', {
+		dateStyle: 'long',
+		timeZone: 'Europe/Paris',
+	}).format(date)
 }
